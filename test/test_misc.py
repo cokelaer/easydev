84d0025e6e82f85ffcb37ca571e9d42f0ad4e009
cokelaer <cokelaer@gmail.com>
2015-06-08 15:51:42 +0100

    add test

--- test/test_misc.py
+++ test/test_misc.py
@@ -1,8 +1,9 @@
-from easydev.misc import get_home, cmd_exists
+from easydev.misc import get_home, cmd_exists, in_ipynb
 from mock import patch
 import nose.tools
 import subprocess
 import os.path
+
 
 def test_get_home():
     get_home()
@@ -12,6 +13,7 @@ def test_get_home():
         mymock.side_effect = ImportError
         get_home()
 
+
 def test_cmd_exists():
     assert cmd_exists('dummy_dummy') == False
     assert cmd_exists('ls') == True
@@ -20,3 +22,6 @@ def test_cmd_exists():
         mymock.side_effect = Exception
         cmd_exists('ls')
 
+
+def test_in_ipynb():
+    assert in_ipynb() == False
