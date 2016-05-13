17c1c280c000d02da3d5ce5f188ad2692ca98aaf
Thomas Cokelaer <cokelaer@gmail.com>
2016-05-13 10:49:05 +0200

    fix test for py3

--- test/test_chunks.py
+++ test/test_chunks.py
@@ -9,6 +9,7 @@ from easydev import split_into_chunks
 
 def test_chunks():
 
-    assert split_into_chunks([0,1,2,3,4,5,6], 2) == [[0, 2, 4, 6], [1, 3, 5]]
-    assert split_into_chunks([0,1,2,3,4,5], 2) == [[0, 2, 4], [1, 3, 5]]
+    assert list(split_into_chunks([0,1,2,3,4,5,6], 2)) == [[0, 2, 4, 6], [1, 3, 5]]
+    assert list(split_into_chunks([0,1,2,3,4,5], 2)) == [[0, 2, 4], [1, 3, 5]]
 
+test_chunks()
