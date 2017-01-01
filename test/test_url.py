8c233031f92c9f89db675f9b7e57742c7728ebf8
Thomas Cokelaer <cokelaer@gmail.com>
2017-01-01 14:23:23 +0100

    fix test

--- test/test_url.py
+++ test/test_url.py
@@ -2,7 +2,6 @@ from easydev import isurl_reachable
 from nose.plugins.attrib import attr
 
 
-@attr('skip')
 def test_isurl():
     assert isurl_reachable("www.google.com") == True
     assert isurl_reachable("http://www.google.com") == True
