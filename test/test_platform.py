d72d9518622237b1f6affb4eb2e8cacecb2496fc
Thomas Cokelaer <cokelaer@gmail.com>
2017-07-09 23:40:56 +0200

    more tests

--- test/test_platform.py
+++ test/test_platform.py
@@ -1,14 +1,9 @@
+import easydev
 from easydev.platform import get_platform, linux_distribution, is_windows
 from easydev.platform import is_linux, is_mac
 
-try:
-    from unittest.mock import patch
-except:
-    from mock import patch
 
-
-
-def test_platform():
+def test_platform(mocker):
 
     assert get_platform() in ['Linux', 'Windows', 'Mac']
 
@@ -18,9 +13,21 @@ def test_platform():
     is_linux()
     is_mac()
 
-
-
-    def func():
+    """def func():
         raise Exception
     with patch("platform.linux_distribution", func):
         get_platform()
+    """
+
+    mocker.patch.object(easydev.platform, "get_platform")
+    easydev.platform.get_platform.return_value = "Windows"
+    assert is_linux() is False
+    assert is_mac() is False
+    assert is_windows() is True
+
+    mocker.patch.object(easydev.platform, "get_platform")
+    easydev.platform.get_platform.return_value = "Mac"
+    assert is_linux() is False
+    assert is_windows() is False
+    assert is_mac() is True
+
