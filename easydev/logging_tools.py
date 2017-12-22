35ca3f744fc6f1e324d78411f7cc0557948fae1f
Thomas Cokelaer <cokelaer@gmail.com>
2017-12-22 22:54:04 +0100

    update logging

--- easydev/logging_tools.py
+++ easydev/logging_tools.py
@@ -14,77 +14,98 @@
 #
 ##############################################################################
 import logging
+import colorlog
 
 __all__ = ["Logging"]
 
 
-class Logging(object):
+colors = {
+    'DEBUG':    'cyan',
+    'INFO':     'green',
+    'WARNING':  'yellow',
+    'ERROR':    'red',
+    'CRITICAL': 'bold_red'}
+
+
+
+class Logging():
     """logging utility.
-
-    When using the logging utility, it works like a singleton.
-    So, once logging level is set, you cannot set it again easily.
-    Here is a class that allows to do that.
-
-    .. warning:: this is a bit of a hack. Maybe this is not a proper solution but
-        it seems to do the job.
 
     ::
 
-        >>> l = Logging("INFO")
+        >>> l = Logging("root", "INFO")
         >>> l.info("test")
         INFO:root:test
         >>> l.level = "WARNING"
         >>> l.info("test")
 
-
     """
-    # I think that we can not inherit from logging.
-    def __init__(self, level):
-        """.. rubric:: constructor
-
-        :param str level: valid levels are ["INFO", "DEBUG", "WARNING",
-            "CRITICAL", "ERROR"]. If set to True, level is internally set to
-            INFO. If set to False, level is seet internally to ERROR.
-        """
-        self._debugLevel = None
-        self.debugLevel = level
-        self.logging = logging
-        self.info = logging.info
-        self.warning = logging.warning
-        self.critical = logging.critical
-        self.error = logging.error
-        self.debug = logging.debug
+    def __init__(self, name="root", level="WARNING"):
+        self.name = name
+        formatter = colorlog.ColoredFormatter(
+             "%(log_color)s%(levelname)-8s[%(name)s]: %(reset)s %(blue)s%(message)s",
+             datefmt=None,
+             reset=True,
+             log_colors=colors,
+             secondary_log_colors={},
+             style='%'
+        )
+        handler = colorlog.StreamHandler()
+        handler.setFormatter(formatter)
+        logger = colorlog.getLogger(self.name)
+        if len(logger.handlers) == 0:
+            logger.addHandler(handler)
+            self._set_level(level)
 
     def _set_level(self, level):
-        valid_level = [True, False, "INFO", "DEBUG", "WARNING", "CRITICAL", "ERROR"]
-        if level is True:
-            level = "INFO"
-        if level is False:
-            level = "ERROR"
-        if level in valid_level:
-            self._debugLevel = level
-        else:
-            raise ValueError("The level of debugging must be in %s " %valid_level)
-        # I'm not sure this is the best solution, but basicConfig can be called
-        # only once and populatse root.handlers list with one instance of
-        # logging.StreamHandler. So, I reset it before calling basicConfig
-        # that effectively changes the logging behaviour
-        logging.root.handlers = []
-        logging.basicConfig(level=self._debugLevel)
+        if isinstance(level, bool):
+            if level is True:
+                level = "INFO"
+            if level is False:
+                level = "ERROR"
+        assert level in ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
+        logging_level = getattr(colorlog.logging.logging, level)
+        colorlog.getLogger(self.name).setLevel(level)
+
     def _get_level(self):
-        return self._debugLevel
-    debugLevel = property(_get_level, _set_level,
-        doc="Read/Write access to the debug level. Must be one of INFO, " + \
-            "DEBUG, WARNING, CRITICAL, ERROR")
-    level = property(_get_level, _set_level,
-        doc="alias to :attr:`~easydev.logging_tools.Logging.debugLevel` (Read-only access)")
+        level = colorlog.getLogger(self.name).level
+        if level == 10:
+            return "DEBUG"
+        elif level == 20:
+            return "INFO"
+        elif level == 30:
+            return "WARNING"
+        elif level == 40:
+            return "ERROR"
+        elif level == 50:
+            return "CRITICAL"
+        else:
+            return level
+    level = property(_get_level, _set_level)
 
-    # Used copy/deepcopy module
-    def __copy__(self):
-        print("WARNING: easydev.logging_tools.__copy__ deprecated. use copy() instead")
-        s = Logging(self.level)
-        return s
+    def debug(self, msg):
+        colorlog.getLogger(self.name).debug(msg)
+    def info(self, msg):
+        colorlog.getLogger(self.name).info(msg)
+    def warning(self, msg):
+        colorlog.getLogger(self.name).warning(msg)
+    def critical(self, msg):
+        colorlog.getLogger(self.name).critical(msg)
+    def error(self, msg):
+        colorlog.getLogger(self.name).error(msg)
 
-    def __deepcopy__(self, memo):
-        s = Logging(self.level)
-        return s
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
