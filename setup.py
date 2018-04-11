ed1e0f71fbe901ecb0afc0f4c7e35dd6d594ab14
Thomas Cokelaer <cokelaer@gmail.com>
2018-04-11 21:47:00 +0200

    increase minor version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import glob
 
 _MAJOR               = 0
 _MINOR               = 9
-_MICRO               = 36
+_MICRO               = 37
 version              = '%d.%d.%d' % (_MAJOR, _MINOR, _MICRO)
 release              = '%d.%d' % (_MAJOR, _MINOR)
 
