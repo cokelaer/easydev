e6445bc371d9ca87cd76566062dbf9ca884a016a
Thomas Cokelaer <cokelaer@gmail.com>
2020-09-11 17:19:08 +0200

    update

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ from setuptools import setup, find_packages
 import glob
 
 _MAJOR               = 0
-_MINOR               = 9
-_MICRO               = 38
+_MINOR               = 10
+_MICRO               = 0
 version              = '%d.%d.%d' % (_MAJOR, _MINOR, _MICRO)
 release              = '%d.%d' % (_MAJOR, _MINOR)
 
@@ -27,11 +27,9 @@ metainfo = {
           'Intended Audience :: Science/Research',
           'License :: OSI Approved',
           'Operating System :: OS Independent',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
           'Topic :: Software Development :: Libraries :: Python Modules'
           ]
     }
